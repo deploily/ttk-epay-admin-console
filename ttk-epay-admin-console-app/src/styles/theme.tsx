58f0db8afd575ab0be39215b7b87950f8a99c5d5
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-05-27 15:12:29 +0000

    initiation

---
+++ ttk-epay-admin-console-app/src/styles/theme.tsx
@@ -0,0 +1,31 @@
+export const theme = {
+    token: {
+      borderRadius: 8,
+      fontSize: 14,
+      fontFamily: "Inter, sans-serif",
+      headingFontSize: {
+        h1: 35,
+        h2: 30,
+        h3: 28,
+        h4: 24,
+        h5: 20,
+        h6: 16,
+      },
+    },
+    components: {
+      Typography: {
+        fontFamily: "Inter, sans-serif",
+        titleMarginBottom: 8,
+      },
+      Rate: {
+        starColor: "rgb(252,50,50)",
+      },
+     
+      algorithm: "dark",
+      Button: {
+        borderColorDisabled: "rgb(76,150,215)",
+        defaultActiveBorderColor: "rgb(76,150,215)",
+        colorText: "rgba(0,0,0,0)",
+      },
+    },
+  };

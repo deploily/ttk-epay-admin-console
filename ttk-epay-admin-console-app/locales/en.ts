77eae190a82e19eda628a252ed500de277d35b15
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-05-28 15:14:22 +0000

    add translations

--- ttk-epay-admin-console-app/locales/en.ts
+++ ttk-epay-admin-console-app/locales/en.ts
@@ -1,7 +1,17 @@
 export default {
-  "connectionComponant.url": "URL",
-   "connectionComponant.secretKey":"Secret key",
-   "connectionComponant.save":"Save",
-   "connectionComponant.goDeploilyConsole":"Go to deploily console",
+  "registration.url": "URL",
+   "registration.secretKey":"Secret key",
+   "registration.save":"Save",
+   "registration.goDeploilyConsole":"Go to deploily console",
+   "invoice.invoice": "Invoice",
+   "invoice.addInvoice": "Add invoice",
+   "invoice.IS_PAID": "Status",
+   "invoice.paid": "Paid",
+   "invoice.unpaid": "Unpaid",
+   "invoice.ORDER_NAME": "Order name",
+   "invoice.NET_AMOUNT": "Net amount",
+   "invoice.CLIENT_CODE": "Client code",
+   "invoice.CLIENT_NAME": "Client name",
+
   
   } as const
--- ttk-epay-admin-console-app/locales/fr.ts
+++ ttk-epay-admin-console-app/locales/fr.ts
@@ -1,7 +1,17 @@
 export default {
-    "connectionComponant.url": "URL",
-    "connectionComponant.secretKey": "Clé secrète",
-    "connectionComponant.save": "Enregistrer",
-    "connectionComponant.goDeploilyConsole": "Accéder à la console de déploiement",
+    "registration.url": "URL",
+    "registration.secretKey": "Clé secrète",
+    "registration.save": "Enregistrer",
+    "registration.goDeploilyConsole": "Accéder à la console de déploiement",
+    "invoice.invoice": "Facture",
+    "invoice.addInvoice": "Ajouterfacture",
+    "invoice.IS_PAID": "Statut",
+    "invoice.paid": "Payé",
+    "invoice.unpaid": "Impayé",
+    "invoice.ORDER_NAME": "Nom de la commande",
+    "invoice.NET_AMOUNT": "Montant net",
+    "invoice.CLIENT_CODE": "Code client",
+    "invoice.CLIENT_NAME": "Nom du client",
+
 
 } as const

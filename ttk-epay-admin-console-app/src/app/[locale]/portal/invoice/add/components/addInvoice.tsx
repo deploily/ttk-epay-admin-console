e58e95ba2739039d8828e5aa251cacfcb2718819
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-06-02 09:39:06 +0000

    remove state anfd add link

--- ttk-epay-admin-console-app/src/app/[locale]/portal/invoice/add/components/addInvoice.tsx
+++ ttk-epay-admin-console-app/src/app/[locale]/portal/invoice/add/components/addInvoice.tsx
@@ -2,27 +2,28 @@
 import { useAppDispatch } from "@/lib/hook";
 import { CustomInvoiceInput } from "@/styles/components/inputStyle";
 import { InvoiceIcon } from "@phosphor-icons/react";
-import {Checkbox, Col, Form,  Row } from "antd";
+import { Checkbox, Col, Form, Row } from "antd";
 import { useScopedI18n } from "../../../../../../../locales/client";
 import Title from "antd/es/typography/Title";
 import { theme } from "@/styles/theme";
 import { CustomPrimaryButton } from "@/styles/components/buttonStyle";
 import { postInvoice } from "@/lib/features/invoice/invoiceThanks";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AddInvoice() {
     const dispatch = useAppDispatch();
     const t = useScopedI18n('invoice')
     const [form] = Form.useForm();
-    const [isPaid, setIsPaid] = useState(false);
+    const router = useRouter();
 
-    
 
-    const handleFinish = (values: any) => {
-        
-        dispatch(postInvoice({  ...values }))
+
+    const handleFinish = async(values: any) => {
+
+        await dispatch(postInvoice({ ...values }))
             .unwrap()
-            
+        router.back()
+
     };
 
     return (
@@ -34,107 +35,107 @@ export default function AddInvoice() {
                         {t("invoice")}
                     </Title>
                 </Col>
-               
+
             </Row>
             <Row>
-                
+
             </Row>
-           
 
-           
-                <>
-                    <Form
-                        form={form}
-                        name="profile-update"
-                        onFinish={handleFinish}
-                        labelCol={{ flex: '110px' }}
-                        labelAlign="left"
-                        labelWrap
-                        wrapperCol={{ flex: 0 }}
-                        colon={false}
-                        style={{ padding: 20 }}
-                    >
-                        <Row gutter={[16, 16]}>
-                            <Col md={12} xs={24}>
-                                <Form.Item label={t('orderId')} name="orderId">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                            <Col md={12} xs={24}>
-                                <Form.Item label={t('orderName')} name="orderName">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                        <Form.Item label={t('netAmount')} name="netAmount">
-                            <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                        </Form.Item>
-                        <Row gutter={[16, 16]}>
-                            <Col md={8} xs={24}>
-                                <Form.Item label={t('invoiceTva')} name="invoiceTva">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                            <Col md={8} xs={24}>
-                                <Form.Item label={t('amountTva')} name="amountTva">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                            <Col md={8} xs={24}>
-                                <Form.Item label={t('amountTtc')} name="amountTtc">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                        <Form.Item label={t('clientCode')} name="clientCode">
-                            <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
-                        </Form.Item>
-                        <Row gutter={[16, 16]}>
-                            <Col md={16} xs={24}>
-                                <Form.Item label={t('clientName')} name="clientName">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                            <Col md={8} xs={24}>
-                                <Form.Item label={t('clientNrc')} name="clientNrc">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                        <Form.Item label={t('clientAdress')} name="clientAdress">
-                            <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                        </Form.Item>
-                        <Form.Item label={t('clientMail')} name="clientMail">
-                            <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                        </Form.Item>
-                        <Row gutter={[16, 16]}>
-                            <Col md={20} xs={24}>
-                                <Form.Item label={t('ClientIdf')} name="ClientIdf">
-                                    <CustomInvoiceInput  style={{ color: theme.token.colorBlack }} />
-                                </Form.Item>
-                            </Col>
-                            <Col md={4} xs={24}>
-                                <Form.Item name="isPaid">
+
+
+            <>
+                <Form
+                    form={form}
+                    name="profile-update"
+                    onFinish={handleFinish}
+                    labelCol={{ flex: '110px' }}
+                    labelAlign="left"
+                    labelWrap
+                    wrapperCol={{ flex: 0 }}
+                    colon={false}
+                    style={{ padding: 20 }}
+                >
+                    <Row gutter={[16, 16]}>
+                        <Col md={12} xs={24}>
+                            <Form.Item label={t('orderId')} name="orderId">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                        <Col md={12} xs={24}>
+                            <Form.Item label={t('orderName')} name="orderName">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Form.Item label={t('netAmount')} name="netAmount">
+                        <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                    </Form.Item>
+                    <Row gutter={[16, 16]}>
+                        <Col md={8} xs={24}>
+                            <Form.Item label={t('invoiceTva')} name="invoiceTva">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                        <Col md={8} xs={24}>
+                            <Form.Item label={t('amountTva')} name="amountTva">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                        <Col md={8} xs={24}>
+                            <Form.Item label={t('amountTtc')} name="amountTtc">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Form.Item label={t('clientCode')} name="clientCode">
+                        <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                    </Form.Item>
+                    <Row gutter={[16, 16]}>
+                        <Col md={16} xs={24}>
+                            <Form.Item label={t('clientName')} name="clientName">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                        <Col md={8} xs={24}>
+                            <Form.Item label={t('clientNrc')} name="clientNrc">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Form.Item label={t('clientAdress')} name="clientAdress">
+                        <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                    </Form.Item>
+                    <Form.Item label={t('clientMail')} name="clientMail">
+                        <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                    </Form.Item>
+                    <Row gutter={[16, 16]}>
+                        <Col md={20} xs={24}>
+                            <Form.Item label={t('ClientIdf')} name="ClientIdf">
+                                <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                            </Form.Item>
+                        </Col>
+                        <Col md={4} xs={24}>
+                            <Form.Item name="isPaid" valuePropName="checked">
                                 <Checkbox>{t("isPaid")}</Checkbox>
-                                </Form.Item>
-                            </Col>
-                        </Row>
+                            </Form.Item>
+                        </Col>
+                    </Row>
 
-                        <Form.Item label={t('productName')} name="productName">
-                            <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
-                        </Form.Item>
+                    <Form.Item label={t('productName')} name="productName">
+                        <CustomInvoiceInput style={{ color: theme.token.colorBlack }} />
+                    </Form.Item>
 
-                        <Form.Item wrapperCol={{ span: 24 }} style={{ textAlign: 'end', marginTop: 20 }}>
-                            <CustomPrimaryButton htmlType="submit">
-                                {t("saveInvoice")}
-                            </CustomPrimaryButton>
-                        </Form.Item>
-                    </Form>
+                    <Form.Item wrapperCol={{ span: 24 }} style={{ textAlign: 'end', marginTop: 20 }}>
+                        <CustomPrimaryButton htmlType="submit">
+                            {t("saveInvoice")}
+                        </CustomPrimaryButton>
+                    </Form.Item>
+                </Form>
 
-                </>
-            
+            </>
 
-           
+
+
 
         </>
     )

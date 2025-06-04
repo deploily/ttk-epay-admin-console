6ddabdfb847c051bb36b8ba258a26bfbfec1445f
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-06-04 15:18:03 +0000

    update middleware

--- ttk-epay-admin-console-app/src/middleware.ts
+++ ttk-epay-admin-console-app/src/middleware.ts
@@ -1,15 +1,57 @@
-// middleware.ts
-import { createI18nMiddleware } from 'next-international/middleware'
-import { NextRequest } from 'next/server'
- 
-const I18nMiddleware = createI18nMiddleware({
-  locales: ['en', 'fr'],
-  defaultLocale: 'en'
-})
- 
-export function middleware(request: NextRequest) {
-  return I18nMiddleware(request)
+import { NextRequest } from "next/server";
+import { createI18nMiddleware } from "next-international/middleware";
+
+export default async function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+  default-src 'self';
+  connect-src 'self' https://pay.deploily.cloud;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+  frame-src 'self' https://pay.deploily.cloud;
+`;
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
+
+  // Step 1: Use the incoming request (example)
+  const defaultLocale = request.headers.get("x-your-custom-locale") || "en";
+
+  // Step 2: Create and call the next-intl middleware (example)
+  const I18nMiddleware = createI18nMiddleware({
+    locales: ['en', 'fr'],
+    defaultLocale: 'en'
+  })
+   
+  const response = I18nMiddleware(request);
+
+  // Step 3: Alter the response (example)
+  response.headers.set("x-your-custom-locale", defaultLocale);
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
+  response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+  response.headers.set("X-Frame-Options", "DENY");
+  response.headers.set("X-Content-Type-Options", "nosniff");
+  response.headers.set("Strict-Transport-Security", "max-age=63072000");
+  return response;
 }
+
  
 export const config = {
   matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)']

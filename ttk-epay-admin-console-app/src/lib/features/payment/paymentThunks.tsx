75bb5c24ecbc37ad20107b22de797d373272f813
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-07-01 14:35:33 +0000

    refactor payment thunks to use TtkEpay class and improve method naming consistency

--- ttk-epay-admin-console-app/src/lib/features/payment/paymentThunks.tsx
+++ ttk-epay-admin-console-app/src/lib/features/payment/paymentThunks.tsx
@@ -1,18 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { payment } from "./data";
 import { RootState } from "@/lib/store";
-const { ttk_epay } = require('@deploily/ttk-epay-nodejs-client');
+const { TtkEpay } = require('@deploily/ttk-epay-nodejs-client');
 
 
 export const fetchPayment = createAsyncThunk(
   "payment/fetchPayment",
   async (data: any, thunkAPI) => {
     const state = thunkAPI.getState() as RootState
-    const client = new ttk_epay(state.registration.registration?.url);
-    console.log("client=== ", client);
+    const client = new TtkEpay(state.registration.registration?.url);
     
     try {
-      const payments = await client.get_payments({
+      const payments = await client.getPayments({
         pageNumber: data.numberPage,
         pageSize: data.pageSize,
         from_date: data.startDate,
@@ -31,28 +29,26 @@ export const fetchPayment = createAsyncThunk(
 export const getPaymentById = createAsyncThunk(
   "payment/getPaymentById",
   async (paymentId: string, thunkAPI) => {
-    try {
-      const response = { status: 200, data: payment };
+    const state = thunkAPI.getState() as RootState
+    const client = new TtkEpay(state.registration.registration?.url);
 
-      if (response.status === 200) {
-        return response.data;
-      } else {
-        return thunkAPI.rejectWithValue("Failed to get payment");
-      }
+    try {
+      const payment = await client.getPaymentById(paymentId);
+      return payment;
     } catch (error: any) {
+      console.error(`Error finding payment: ${error.message}`);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 )
 
-
 export const savePdfReceipt = createAsyncThunk(
   "payment/savePdfReceipt",
   async (satimOrderId: string, thunkAPI) => {
     const state = thunkAPI.getState() as RootState
-    const client = new ttk_epay(state.registration.registration?.url);
+    const client = new TtkEpay(state.registration.registration?.url);
     try {
-      const pdfData = await client.get_pdf_recipt(satimOrderId); // Assure-toi que c’est un `Blob` ou `ArrayBuffer`
+      const pdfData = await client.getPdfRecipt(satimOrderId); 
 
       const blob = new Blob([pdfData], { type: 'application/pdf' });
       const url = window.URL.createObjectURL(blob);

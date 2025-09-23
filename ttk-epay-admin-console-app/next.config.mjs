becf57505d2e9f815a1d6f69882ee5a683a6bea2
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-09-23 15:13:25 +0000

    update configuration

--- ttk-epay-admin-console-app/next.config.mjs
+++ ttk-epay-admin-console-app/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
+    output: "export",  // <=== enables static exports
+    trailingSlash: true,
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // <== disables Next.js image optimization
+      },
 };
 
 export default nextConfig;

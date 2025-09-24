d6293dbd28b27d49191d2d11673e00d4a2bcfbe7
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-09-24 15:00:17 +0000

    Push out static files.

---
+++ ttk-epay-admin-console-app/out/_next/static/6fzyGgzvTzyhrMejTRi1z/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fportal","\u002F[locale]\u002Fportal\u002Finvoice","\u002F[locale]\u002Fportal\u002Finvoice\u002Fadd","\u002F[locale]\u002Fportal\u002Fpayment"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

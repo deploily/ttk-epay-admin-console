c106be568fba3e80dcfe63b6a12b48a4319aa207
ziadikheira <92316180+ziadikheira@users.noreply.github.com>
2025-09-24 15:34:20 +0100

    Update next.config.mjs

--- ttk-epay-admin-console-app/next.config.mjs
+++ ttk-epay-admin-console-app/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/ttk-epay-admin-console",
     output: "export",  
     trailingSlash: true,
     reactStrictMode: true,

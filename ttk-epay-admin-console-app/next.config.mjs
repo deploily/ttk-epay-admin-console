3b7ca37d3c09dcb9ce7db6376b323b1bb552aa20
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-09-25 08:58:08 +0000

    Back to initial setup for publishing to GitHub Pages.

--- ttk-epay-admin-console-app/next.config.mjs
+++ ttk-epay-admin-console-app/next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/ttk-epay-admin-console",
-    output: "export",  
-    // trailingSlash: true,
+    output: "export",
+    trailingSlash: true,
     reactStrictMode: true,
-    assetPrefix:'/ttk-epay-admin-console/',
-    images: {
-        unoptimized: true,
-    },
+    
 };
 
 export default nextConfig;
